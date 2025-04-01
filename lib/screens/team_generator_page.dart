d639926db2f4d2b7a286506d762e31cd5955a829
LP <luhis9929@gmail.com>
2025-04-01 00:56:51 -0500

    agregando fondo a la pantalla principal

--- lib/screens/team_generator_page.dart
+++ lib/screens/team_generator_page.dart
@@ -1,4 +1,3 @@
-// lib/screens/team_generator_page.dart
 import 'package:flutter/material.dart';
 import 'players_generate_screen.dart';
 import 'match_screen.dart';
@@ -10,56 +9,80 @@ class TeamGeneratorPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("⚽ Team Generator by LP ⚽" , style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.green,
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/fondoP.jpg"),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.white.withOpacity(0.5),
+            BlendMode.dstATop,
+          ),
+        ),
       ),
-      body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => PlayersGenerateScreen()),
-                );
-              },
-              child: Text("Jugadores"),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => GeneratedTeamsScreen()),
-                );
-              },
-              child: Text("Ver Equipos Guardados"),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => MatchScreen()),
-                );
-              },
-              child: Text("Ir a Partido"),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => PaymentCalculatorScreen()),
-                );
-              },
-              child: Text("Pagos"),
-            ),
-          ],
+      child: Scaffold(
+        backgroundColor: Colors.transparent, // Para ver el fondo
+        appBar: AppBar(
+          title: Center(child: Text("⚽ PICHANGA by LP ⚽", style: TextStyle(color: Colors.white))),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 200, // Tamaño fijo para todos los botones
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => PlayersGenerateScreen()),
+                    );
+                  },
+                  child: Text("Jugadores"),
+                ),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => GeneratedTeamsScreen()),
+                    );
+                  },
+                  child: Text("Ver Equipos Guardados"),
+                ),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => MatchScreen()),
+                    );
+                  },
+                  child: Text("Ir a Partido"),
+                ),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => PaymentCalculatorScreen()),
+                    );
+                  },
+                  child: Text("Pagos"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
